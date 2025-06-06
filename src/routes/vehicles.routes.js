685d8c14d15f1823de18534952bac8b409df00ce
Alexis-Scz <alexissoucaze@yahoo.fr>
2025-06-07 00:01:31 +0200

    les "/..." en "/"

--- src/routes/vehicles.routes.js
+++ src/routes/vehicles.routes.js
@@ -9,12 +9,11 @@ import {
 } from "../controller/vehicles.controller.js";
 const router = Router();
 
-// router.post("/", createVehicles);
+router.post("/", createVehicles);
 
-router.get("/list", getCars);
+router.get("/", getCars);
 router.get("/ent", getEntretien);
 router.get("/list_piece", listPiece);
-router.post("/create", createVehicles);
 router.post("/create_ent", createEntretien);
 router.post("/create_piece", createPiece);
 export default router;
